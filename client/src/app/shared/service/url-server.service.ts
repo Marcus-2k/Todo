aef735f6699340740dc29f22e0574b0d6cdd06bb
Маркус <marcus.petrihen@gmail.com>
2022-07-21 15:23:16 +0300

    master

--- client/src/app/shared/service/url-server.service.ts
+++ client/src/app/shared/service/url-server.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class UrlServerService {
   constructor() {}
 
   // private urlStart: string = 'http://';
-  private HOST: string = 'localhost';
-  private PORT: string = ':5000';
+  private HOST: string = "localhost";
+  private PORT: string = ":5000";
   // private urlAPI: string = '/api/';
 
   urlFull: string = `http://${this.HOST}${this.PORT}/api/`;
